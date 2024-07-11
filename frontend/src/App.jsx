4a365d78e59b69af19143db8e8dcf04228cf3e05
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-07-12 00:21:54 +0530

    done basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Layout() {
   return (
     <>
       
+      <Outlet />
     </>
-  )
+  );
+}
+
+const appRouting=createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout /> ,
+      children:[
+        {
+          path: "/"
+        }
+      ]
+    }
+  ]
+)
+function App() {
+  return (
+    <RouterProvider router={appRouting} />
+  );
 }
 
 export default App
