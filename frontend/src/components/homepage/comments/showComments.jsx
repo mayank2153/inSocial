2e118423e335949d7796efbb053d60862356a04f
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-07-18 21:03:54 +0530

    made fetchPost by category and solved all the existing errors

--- frontend/src/components/homepage/comments/showComments.jsx
+++ frontend/src/components/homepage/comments/showComments.jsx
@@ -3,13 +3,11 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import CommentDisplay from "./commentDisplay";
 
-const url = import.meta.env.VITE_BASE_URL;
+const url = import.meta.env.VITE_BASE_URL || `http://localhost:8000/`;
 
 const ShowComments = () => {
   const { postId } = useParams();
   const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -20,34 +18,22 @@ const ShowComments = () => {
           },
           withCredentials: true,
         });
-        console.log(response.data.data)
+        
         setComments(response.data.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
+      } 
+      catch (error) {
+        console.error(error)
+      } 
     };
 
     fetchComments();
   }, [postId]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error fetching comments: {error}</div>;
-  }
-
-  if (!comments.length) {
-    return <div>No comments to display</div>;
-  }
 
   return (
     <div>
       <h3>Comments</h3>
-      {comments.map((comment) => (
+      {comments && comments.length>0 && comments.map((comment) => (
         <div key={comment._id} className="p-2 m-2 border-b">
           <CommentDisplay {...comment} />
         </div>
