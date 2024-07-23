6ea84b37a63ab3a6a4f4f4c425aa5b7703a2991d
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-07-23 12:39:21 +0530

    built backend for search functionality

---
+++ backend/src/controllers/search.controller.js
@@ -0,0 +1,21 @@
+import { Post } from "../models/post.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const searchUsersAndPosts=asyncHandler(async(req,res)=>{
+    const {query}=req.query;
+    try {
+        const posts=await Post.find({title: {$regex:query,$options:'i'}});
+        const users=await User.find({userName: {$regex:query,$options:'i'}});
+        return res.status(200).json(
+            new ApiResponse(200,{posts,users},"Search Successfull")
+        )
+    } catch (error) {
+        throw new ApiError(500,error);
+    }
+})
+export {
+    searchUsersAndPosts
+}
