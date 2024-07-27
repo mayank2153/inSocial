e3ed325073e30647dbf8b68216a3144d5035a754
Rahul Bhardwaj <rbh97995@gmail.com>
2024-07-27 20:33:01 +0530

    minor UI Changes

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -24,35 +24,35 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-gradient-to-b from-[#1e0832] via-[#290f42] to-[#452762] fixed w-full z-10">
-      <div className="max-w-7xl mx-auto px-5 lg:px-10">
+    <header className="bg-[#1e0832] fixed w-full z-10 border-b-2 border-black">
+      <div className="max-w-7xl mx-auto ">
         <nav className="flex items-center h-16 lg:h-20 justify-between">
-          <div className="flex-shrink-0">
+          <div className="flex items-center justify-end -ml-20">
             <Link to="/" title="Home" className="flex">
               <img className="w-auto h-8 lg:h-10 hidden lg:block" src={logo} alt="Logo" />
               <img className={`w-auto h-8 lg:h-10 lg:hidden ${isSearchOpen ? 'hidden' : 'block'}`} src={logo_img} alt="Logo" />
             </Link>
           </div>
 
-          {isAuthenticated ? (
-            <>
-              <div className="hidden lg:flex lg:items-center lg:space-x-2">
-                <SearchBar />
-                <a href="#" title="Notifications" className="text-white hover:text-blue-600">
-                  <IoIosNotificationsOutline size={30} />
-                </a>
-                <Link to="/createPost" className="px-4 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">Create</Link>
-                <div className="relative">
-                  <img
-                    src={userProfileImage}
-                    alt='user Profile'
-                    className='w-12 h-12 rounded-full object-cover cursor-pointer'
-                    onClick={toggleProfile}
-                  />
-                  {isProfileOpen && <ProfilePage />}
-                </div>
-              </div>
+          <div className="flex-grow hidden lg:flex lg:items-center lg:justify-center">
+            <SearchBar />
+          </div>
 
+          {isAuthenticated ? (
+            <div className="flex items-center justify-end space-x-4 -mr-20">
+              <a href="#" title="Notifications" className="text-white hover:text-blue-600">
+                <IoIosNotificationsOutline size={30} />
+              </a>
+              <Link to="/createPost" className="px-4 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">Create</Link>
+              <div className="relative">
+                <img
+                  src={userProfileImage}
+                  alt="user Profile"
+                  className="w-12 h-12 rounded-full object-cover cursor-pointer"
+                  onClick={toggleProfile}
+                />
+                {isProfileOpen && <ProfilePage />}
+              </div>
               <button
                 type="button"
                 className="inline-flex p-2 text-white rounded-md lg:hidden"
@@ -60,18 +60,18 @@ const Header = () => {
               >
                 <IoIosSearch size={25} />
               </button>
-              <div className={`lg:hidden px-4 py-2 ${isSearchOpen ? 'hidden' : 'block'} ml-4`}>
+              <div className={`lg:hidden py-2 ${isSearchOpen ? 'hidden' : 'block'}`}>
                 <img
                   src={userProfileImage}
-                  alt='user Profile'
-                  className='w-12 h-12 rounded-full object-cover cursor-pointer'
+                  alt="user Profile"
+                  className="w-12 h-12 rounded-full object-cover cursor-pointer"
                   onClick={toggleProfile}
                 />
                 {isProfileOpen && <ProfilePage />}
               </div>
-            </>
+            </div>
           ) : (
-            <>
+            <div className="flex items-center space-x-4 mr-10">
               <SearchBar />
               <Link to="/login" className="px-4 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 lg:hidden">
                 Login
@@ -79,7 +79,7 @@ const Header = () => {
               <Link to="/login" className="hidden lg:block px-4 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">
                 Login
               </Link>
-            </>
+            </div>
           )}
         </nav>
       </div>
