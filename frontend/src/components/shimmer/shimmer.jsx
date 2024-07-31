c72275157732b054ebe82d878722f2cae1f90828
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-07-31 12:43:05 +0530

    made shimmer ui

---
+++ frontend/src/components/shimmer/shimmer.jsx
@@ -0,0 +1,13 @@
+import ShimmerCard from "../shimmer/shimmerCard.jsx"
+export const Shimmer=()=>{
+    return(
+        <div className="w-full bg-[#0d1114] flex justify-center flex-col items-center">
+
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+        </div>
+    )
+}
+export default Shimmer
