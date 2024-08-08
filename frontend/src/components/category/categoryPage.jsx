35c7c3ceacd39caee49453bff77f68dd23a5d287
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-08 16:45:10 +0530

    "Added socket.io to backend, ; modified authjwt.middleware.js to remove console logs; updated user.model.js to include conversations field; added conversation and message models"

--- frontend/src/components/category/categoryPage.jsx
+++ frontend/src/components/category/categoryPage.jsx
@@ -12,7 +12,7 @@ const CategoryPage = () => {
   const likedCategories_redux = useSelector((state) => state.likedCategories?.likesCategory);
   const userData = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
-
+  console.log("into registered")
   const fetchCategories = async () => {
     try {
       const response = await axios.get(`${url}category/category`);
@@ -20,6 +20,7 @@ const CategoryPage = () => {
         const fetchedCategories = response.data.categories;
         setCategories(fetchedCategories);
       }
+      console.log(response)
     } catch (error) {
       console.log('There seems to be an error in fetching Categories:', error);
     }
@@ -27,6 +28,7 @@ const CategoryPage = () => {
 
   useEffect(() => {
     fetchCategories();
+    console.log("cstegoty",categories)
   }, []);
 
   const handleCategorySelect = (categoryId) => {
