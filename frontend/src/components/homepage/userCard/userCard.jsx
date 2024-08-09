c80466a5b4cfeee62a0f8f979744f663efe3ec68
Rahul Bhardwaj <rbh97995@gmail.com>
2024-08-09 18:37:56 +0530

    changes some ui and introduced new feature settings

--- frontend/src/components/homepage/userCard/userCard.jsx
+++ frontend/src/components/homepage/userCard/userCard.jsx
@@ -1,6 +1,6 @@
 const UserCard=({avatar,bio,userName,_id})=>{
     return(
-        <div className="min-h-20 border-t-2 border-slate-300 py-2 px-4 hover:bg-slate-300">
+        <div className="min-h-20 border-b-2 border-gray-600 py-2 px-4 hover:bg-gray-900 hover:cursor-pointer">
             <div className="owner-info flex items-center mb-4 gap-4">
               <img
                 src={avatar}
@@ -8,8 +8,8 @@ const UserCard=({avatar,bio,userName,_id})=>{
                 className="w-10 h-10 rounded-full mr-2"
               />
               <div>
-                <div className="font-bold">{userName}</div>
-                <div className="text-sm">{bio}</div>
+                <div className="text-[16px] text-slate-200">{userName}</div>
+                <div className="text-sm text-slate-200">{bio}</div>
 
               </div>
             </div>
