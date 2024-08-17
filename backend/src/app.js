c2fb41329a7d66a07fc2691153eb467dda872526
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-17 18:19:00 +0530

    "Updated CORS configuration, post controller, and create post component"

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,6 +22,7 @@ app.use(passport.session());
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     })
 )
