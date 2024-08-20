2b3e4206b66fa5d061268677e7640622ced27ef7
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-20 15:17:22 +0530

    "Update session cookie to require secure connection"

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },
+    cookie: { secure: true },
 }));
 
 app.use(passport.initialize());
