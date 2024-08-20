8d4da9ec57a808a61757778dd2070f1d26e0f6db
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-20 15:04:31 +0530

    initialized new branch for uploading backend on render

--- backend/src/middlewares/authjwt.middleware.js
+++ backend/src/middlewares/authjwt.middleware.js
@@ -5,20 +5,20 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
-        // console.log("1");
+        console.log("1");
 
         const token = req.cookies?.accessToken || (req.header("Authorization")?.replace("Bearer ", "") ?? '');
         
-        // console.log("2");
-        // console.log("req.cookies:", req.cookies);
-        // console.log("Authorization header:", req.header("Authorization"));
-        // console.log("Token:", token);
+        console.log("2");
+        console.log("req.cookies:", req.cookies);
+        console.log("Authorization header:", req.header("Authorization"));
+        console.log("Token:", token);
 
-        // console.log("3");
+        console.log("3");
         if (!token) {
             throw new ApiError(401, "Unauthorized Access: No token provided");
         }
-        // console.log("4");
+        console.log("4");
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken?.id).select("-password -refreshToken");
