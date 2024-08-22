fcfe76a24a69a8023899327792080fcc1331f8c0
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-22 18:53:35 +0530

    made the ui responisve of header,homepage,postCard, right section and also deleted the posts of deleted users

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -23,22 +23,24 @@ const HomePage = () => {
   }
   console.log(items);
   return (
-    <div className="flex flex-col  bg-[#0d1114] w-full max-h-screen overflow-y-auto items-center no-scrollbar py-8">
-      <div className="rounded-2xl bg-[#13181d] p-1 py-2">
-      {
-        items && items.data && items.data.length > 0 ? (
-          items.data.map((item) => (
-            <div className="border-slate-200 border-b  w-fit mb-4 " key={item._id}>
-              
-              <PostCard  {...item}  />
-            </div>
-          ))
-        ) : (
-          <div>No posts available</div>
-        )
-      }
-      </div>
-    </div>
+    <div className="flex flex-col bg-[#0d1114] w-full max-h-screen overflow-y-auto items-center no-scrollbar py-8 ">
+  <div className="w-full max-w-screen-sm px-2">
+  <div className="rounded-2xl bg-[#13181d] p-1 py-2 space-y-4">
+    {
+      items && items.data && items.data.length > 0 ? (
+        items.data.map((item) => (
+          <div className="border-slate-200 border-b w-full mb-4 box-border" key={item._id}>
+            <PostCard {...item} />
+          </div>
+        ))
+      ) : (
+        <div>No posts available</div>
+      )
+    }
+  </div>
+</div>
+
+</div>
   );
 };
 
