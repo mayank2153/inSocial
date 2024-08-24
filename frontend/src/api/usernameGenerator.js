a9ec47ccb598aa2f315dacdcafc255e0ee5e97bb
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-25 01:12:25 +0530

    added google gemini for generating unique usernames

---
+++ frontend/src/api/usernameGenerator.js
@@ -0,0 +1,21 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_API_KEY);
+
+async function usernameGenerator() {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const result = await model.generateContent([
+      "Generate one unique and meaningful anonymous username that is easy to remember and suitable for a diverse range of users. The username should be a combination of words or phrases that evoke a positive or neutral idea, concept, or imagery. It should not include any real names or common identifiers.Username should not be greater than 15 characters. Ensure the username is creative, meaningful, and distinctive.Return only the username without any additional explanation."
+    ]);
+    console.log("unique username in gemini:",result.response)
+    return result.response.text();
+  } catch (error) {
+    console.error("Error generating username:", error);
+    return ""
+    
+  }
+}
+export default usernameGenerator;
+
+
