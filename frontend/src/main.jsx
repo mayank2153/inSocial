21f3295b4786c71f678b2c066316270db2470565
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-29 11:58:49 +0530

    Remove NotificationContainer component from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import { NotificationContainer } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import { Provider } from 'react-redux';
 import { store } from './utils/store.jsx';
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}> 
     <App />
-    <NotificationContainer />
     </Provider>
   </React.StrictMode>,
 )
