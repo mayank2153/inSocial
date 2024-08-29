14e9fc39b9064372335419ecc9018fdd7c4e4b11
Rahul Bhardwaj <rbh97995@gmail.com>
2024-08-29 19:37:51 +0530

    feat: Add toast notifications for success and error messages in forgetPassword and resetPassword components

--- frontend/src/api/forgetPassword.js
+++ frontend/src/api/forgetPassword.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 const url = import.meta.env.VITE_BASE_URL || 'http://localhost:8000/';
+import toast from 'react-hot-toast';
 
 export const forgetPassword = async(email) => {
     try {
+        
         const response = await axios.post( `${url}users/forgetPassword`, {
             email        
         });
-        console.log('request for changing email sent', response);
+        toast.success(response?.data?.message)    
+
         
     } catch (error) {
-        console.log('there seems to be a problem in fetching email', error);
-        
+        toast.error(error?.response?.data?.message)
     }
 };
