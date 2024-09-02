cf101868d93b29ea4890a34311ac2c007ca37f21
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-09-02 18:45:15 +0530

    "Update session cookie maxAge from 15 minutes to 1 hour"

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,7 +43,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        maxAge: 15 * 60 * 1000,
+        maxAge: 60 * 60 * 1000,
         sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
         secure:process.env.NODE_ENV==="Development"?false:true,
     },
